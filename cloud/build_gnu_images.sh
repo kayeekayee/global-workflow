820a049f64349398391cd219eb69831a6fee0a2d
Daniel Abdi <daniel.abdi@noaa.gov>
2020-06-28 18:02:24 -0600

    Clone global-workflow from container using secrets.

--- cloud/build_gnu_images.sh
+++ cloud/build_gnu_images.sh
@@ -5,15 +5,18 @@ if ! [ -x "$(command -v docker)" ]; then
   exit 1
 fi
 
-if ! [ -d global-workflow ]; then
-  ./scripts/checkout_gfs.sh
-fi
-
 REPO=${REPO:-dshawul}
+GERRIT_ID=${GERRIT_ID:-gerrit}
+SSH_CONFIG=${SSH_CONFIG:-${HOME}/.ssh/config}
+
+export DOCKER_BUILDKIT=1
 
 #build image
 build_image() {
-    docker build --build-arg REPO=${REPO} -t ${1} -f ${2} .
+    docker build --ssh default --secret id=${GERRIT_ID},src=${SSH_CONFIG} \
+                 --build-arg REPO=${REPO} \
+                 --build-arg GERRIT_ID=${GERRIT_ID} \
+                 -t ${1} -f ${2} .
 }
 
 #netcdf
@@ -26,11 +29,6 @@ IMAGE_NAME=${REPO}/esmf-gnu
 DOCKER_FILE=Dockerfiles/gnu/Dockerfile-esmf
 build_image ${IMAGE_NAME} ${DOCKER_FILE}
 
-#fv3
-IMAGE_NAME=${REPO}/fv3-gnu
-DOCKER_FILE=Dockerfiles/gnu/Dockerfile-fv3
-build_image ${IMAGE_NAME} ${DOCKER_FILE}
-
 #nceplibs
 IMAGE_NAME=${REPO}/nceplibs-gnu
 DOCKER_FILE=Dockerfiles/gnu/Dockerfile-nceplibs
@@ -40,3 +38,9 @@ build_image ${IMAGE_NAME} ${DOCKER_FILE}
 IMAGE_NAME=${REPO}/gfs-gnu
 DOCKER_FILE=Dockerfiles/gnu/Dockerfile-gfs
 build_image ${IMAGE_NAME} ${DOCKER_FILE}
+
+#fv3
+IMAGE_NAME=${REPO}/fv3-gnu
+DOCKER_FILE=Dockerfiles/gnu/Dockerfile-fv3
+build_image ${IMAGE_NAME} ${DOCKER_FILE}
+
--- cloud/build_intel_images.sh
+++ cloud/build_intel_images.sh
@@ -5,16 +5,21 @@ if ! [ -x "$(command -v docker)" ]; then
   exit 1
 fi
 
-if ! [ -d global-workflow ]; then
-  ./scripts/checkout_gfs.sh
-fi
+REPO=${REPO:-dshawul}
+GERRIT_ID=${GERRIT_ID:-gerrit}
+SSH_CONFIG=${SSH_CONFIG:-${HOME}/.ssh/config}
+
+export DOCKER_BUILDKIT=1
 
 INTEL_COMP_DIR=/data/intel
 COMP=${COMP:-intel}
-REPO=${REPO:-dshawul}
 
 #loads intel compiler from host and builds image with it
 build_image() {
+    docker build --ssh default --secret id=${GERRIT_ID},src=${SSH_CONFIG} \
+                 --build-arg REPO=${REPO} \
+                 --build-arg GERRIT_ID=${GERRIT_ID} \
+                 -t ${1} -f ${2} .
     docker build --build-arg REPO=${REPO} -t ${1} -f ${2} .
     docker run --name temp -v ${INTEL_COMP_DIR}:${INTEL_COMP_DIR} -d -it ${1} bash
     docker exec -it temp /bin/bash -c "${3}"
@@ -34,12 +39,6 @@ DOCKER_FILE=Dockerfiles/intel/Dockerfile-esmf
 COMMAND="cd /opt; source intel_comp.sh; COMP=${COMP} ./build_esmf.sh"
 build_image ${IMAGE_NAME} ${DOCKER_FILE} "${COMMAND}"
 
-#fv3
-IMAGE_NAME=${REPO}/fv3-intel
-DOCKER_FILE=Dockerfiles/intel/Dockerfile-fv3
-COMMAND="cd /opt; source intel_comp.sh; COMP=${COMP} ./build_fv3.sh; ./copy_deps.sh"
-build_image ${IMAGE_NAME} ${DOCKER_FILE} "${COMMAND}"
-
 #nceplibs
 IMAGE_NAME=${REPO}/nceplibs-intel
 DOCKER_FILE=Dockerfiles/intel/Dockerfile-nceplibs
@@ -50,5 +49,11 @@ build_image ${IMAGE_NAME} ${DOCKER_FILE} "${COMMAND}"
 IMAGE_NAME=${REPO}/gfs-intel
 DOCKER_FILE=Dockerfiles/intel/Dockerfile-gfs
 COMMAND="cd /opt; source intel_comp.sh; ./patch_gfs.sh; cd global-workflow/sorc; ./build_all.sh; ./link_fv3gfs.sh emc linux.intel"
+build_image ${IMAGE_NAME} ${DOCKER_FILE} "${COMMAND}"
+
+#fv3
+IMAGE_NAME=${REPO}/fv3-intel
+DOCKER_FILE=Dockerfiles/intel/Dockerfile-fv3
+COMMAND="cd /opt; source intel_comp.sh; ./copy_deps.sh"
 build_image ${IMAGE_NAME} ${DOCKER_FILE} "${COMMAND}"
 
