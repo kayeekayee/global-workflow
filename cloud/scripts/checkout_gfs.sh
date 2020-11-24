cce44d309d8c4c0bd4fe8946f9853e3de7a64f41
Daniel Abdi <daniel.abdi@noaa.gov>
2020-11-24 15:17:13 -0700

    Update from GSD to GSL.

--- cloud/scripts/checkout_gfs.sh
+++ cloud/scripts/checkout_gfs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-git clone --recursive https://github.com/NOAA-GSD/global-workflow.git
+git clone --recursive https://github.com/NOAA-GSL/global-workflow.git
 cd global-workflow
 git checkout feature/cloud-develop
 cd sorc
