146bd4c9ec8c2e6d646572328e97cd55309d730c
kayee.wong <kayee.wong@noaa.gov>
2024-09-20 01:13:55 +0000

    Update experiment directory.

--- workflow/rt_c3_mynn.sh
+++ workflow/rt_c3_mynn.sh
@@ -1,12 +1,14 @@
+#!/bin/sh
 USER=role.rtfim
-GITDIR=${HOME}/UFS-CAMsuite/                                     ## where your git checkout is located
+#GITDIR=${HOME}/UFS-CAMsuite/                                     ## where your git checkout is located
+GITDIR=/scratch2/BMC/gsd-fv3-dev/KaYee.Wong/global/test/Merge_gsl_ufs_dev_to_gsl_ufsrtdev1/
 COMROT=$GITDIR/FV3GFSrun                                         ## default COMROT directory
 EXPDIR=$GITDIR/FV3GFSwfm                                         ## default EXPDIR directory
 ICSDIR=/scratch1/BMC/gsd-fv3/rtruns/FV3ICS_L127
 
 PSLOT=rt_v17p8_ugwpv1_c3_mynn
-IDATE=2024011400
-EDATE=2024011400
+IDATE=2024091800
+EDATE=2024091800
 RESDET=768               ## 96 192 384 768
 
 ### gfs_cyc 1  00Z only;  gfs_cyc 2  00Z and 12Z
